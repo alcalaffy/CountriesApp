87986672bcdc280847bfbb691f8da12ded82c88a
alcalaffy <81399287+alcalaffy@users.noreply.github.com>
2024-06-09 15:34:59 -0600

    using counties api

---
+++ src/app/countries/pages/by-capital-page/by-capital-page.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { CountriesService } from '../../services/countries.service';
+import { Country } from '../../interfaces/country.interface';
+
+@Component({
+  selector: 'app-by-capital-page',
+  templateUrl: './by-capital-page.component.html',
+  styleUrl: './by-capital-page.component.css'
+})
+export class ByCapitalPageComponent {
+  public countries:Country[]=[];
+  constructor(private countryService:CountriesService){}
+
+searchByCapital(term:string):void{
+  this.countryService.searchCapital(term).subscribe(
+    countries=>{
+      this.countries=countries;
+    }
+  );
+  console.log('desdeByCapital: ',term);
+}
+}
