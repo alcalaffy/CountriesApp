e2333ac526ddd0c7261cdbd97cb61ad921590cc0
alcalaffy <81399287+alcalaffy@users.noreply.github.com>
2024-07-09 15:05:39 -0600

    Adding debouncer in searchbox component

--- src/app/countries/services/countries.service.ts
+++ src/app/countries/services/countries.service.ts
@@ -8,41 +8,25 @@ export class CountriesService {
   private apiUrl:string='https://restcountries.com/v3.1';
 
   constructor(private http: HttpClient) { }
-
+  private getCountriesReq(url:string):Observable<Country[]>{
+    return this.http.get<Country[]>(url)
+      .pipe(
+        catchError(()=>of ([])),
+      );
+  }
   searchCapital(term:string):Observable<Country[]>{
     const url:string=`${this.apiUrl}/capital/${term}`;
-    return this.http.get<Country[]>(url)
-    .pipe(
-      catchError(err=>{
-        console.log(err)
-        return of ([])
-      }
-    )
-    );
+    return this.getCountriesReq(url);
   }
 
   searchCountry(term:string):Observable<Country[]>{
     const url:string=`${this.apiUrl}/name/${term}`;
-    return this.http.get<Country[]>(url)
-    .pipe(
-      catchError(err=>{
-        console.log(err)
-        return of ([])
-      }
-    )
-    );
+    return this.getCountriesReq(url);
   }
 
   searchRegion(term:string):Observable<Country[]>{
     const url:string=`${this.apiUrl}/region/${term}`;
-    return this.http.get<Country[]>(url)
-    .pipe(
-      catchError(err=>{
-        console.log(err)
-        return of ([])
-      }
-    )
-    );
+    return this.getCountriesReq(url);
   }
 
   searchCountryByAlphaCode(code:string):Observable<Country | null>{
--- src/app/shared/components/search-box/search-box.component.ts
+++ src/app/shared/components/search-box/search-box.component.ts
@@ -1,18 +1,37 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Subject, debounceTime } from 'rxjs';
 
 @Component({
   selector: 'shared-search-box',
   templateUrl: './search-box.component.html',
   styleUrl: './search-box.component.css'
 })
-export class SearchBoxComponent {
+export class SearchBoxComponent implements OnInit{
   @Input()
   public placeholder:string='';
+
+  public debouncer:Subject<string>=new Subject<string>();
 
   @Output()
   public onValue=new EventEmitter<string>();
 
+  @Output()
+  public onDebaunce=new EventEmitter<string>();
+
+  ngOnInit(): void {
+    this.debouncer
+    .pipe(
+      debounceTime(300)
+    )
+    .subscribe(value=>{
+      this.onDebaunce.emit(value);
+    });
+  }
   emitValue(value:string){
     this.onValue.emit(value);
+  }
+
+  onKeyPress(term:string){
+    this.debouncer.next(term);
   }
 }
